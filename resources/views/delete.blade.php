ed98aa82aa50aa4d2cde78e9a9282d5b9ea669b6
JulietteFynt <71647378+JulietteFynt@users.noreply.github.com>
2021-06-28 21:22:31 +0200

    Add files via upload

---
+++ resources/views/delete.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/layout')
+
+@section('title', 'Task Details')
+
+@section('main')
+
+        <div class="card">
+            <p>The task number {{ $id }} has been successfully deleted.</p>
+        </div>
+
+@endsection
