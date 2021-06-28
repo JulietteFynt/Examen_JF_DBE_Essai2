5b585c5b06c5bc40919ea4cb466d3055d6ce6b94
JulietteFynt <71647378+JulietteFynt@users.noreply.github.com>
2021-06-28 21:23:16 +0200

    Add files via upload

---
+++ storage/framework/views/497cefc7c031d98d1eda8a8b6fac45a608c98359.php
@@ -0,0 +1,19 @@
+
+
+<?php $__env->startSection('title', 'Task Details'); ?>
+
+<?php $__env->startSection('main'); ?>
+        <div class="card">
+            <div class="card-content">
+                <div class="content">
+                    <h3><?php echo e($tasks->title); ?></h3>
+                    <p><?php echo e($tasks->description); ?></p>
+                    <p><?php echo e(\Carbon\Carbon::parse($tasks->created_at)->format('l d F Y, H:m')); ?></p>
+                </div>
+                <div class="card-footer">
+                    <a href="/task/<?php echo e($tasks->id); ?>/delete" class="card-footer-item">Delete Task></a>
+                </div>
+            </div>
+        </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts/layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\fayno\Documents\Semestre 4\Back-end\Examen_JF\resources\views/showtask.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a3ab2bce39c948492c518c0c540a09c0fd5d1420.php
@@ -0,0 +1,12 @@
+
+
+<?php $__env->startSection('title', 'Task Details'); ?>
+
+<?php $__env->startSection('main'); ?>
+
+        <div class="card">
+            <p>The task number <?php echo e($id); ?> has been successfully deleted.</p>
+        </div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts/layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\fayno\Documents\Semestre 4\Back-end\Examen_JF\resources\views/delete.blade.php ENDPATH**/ ?>
